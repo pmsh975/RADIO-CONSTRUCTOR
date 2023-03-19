8640f16e030647e6d0e0a45f508cdacf2ca249d5
pavel shuvalov <pmshuv975@gmail.com>
2023-03-19 21:13:33 +0300

    new constuctors added

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -3,9 +3,20 @@ package ru.netology.radio;
 public class Radio {
     private int currentStation;
     private int currentVolume;
+    private int maxStation;
+
+    public Radio() {
+        maxStation = 9;
+
+    }                          // конструктор по умолчанию
+
+    public Radio(int stationsCount) {
+        maxStation = stationsCount - 1;
+
+    }                         // конструктор, который принимает количество станций
 
     public void next() {
-        if (currentStation != 9) {
+        if (currentStation != maxStation) {
             currentStation++;
         } else {
             currentStation = 0;
@@ -17,7 +28,7 @@ public class Radio {
         if (currentStation != 0) {
             currentStation--;
         } else {
-            currentStation = 9;
+            currentStation = maxStation;
         }
 
     }
@@ -47,7 +58,7 @@ public class Radio {
         if (currentStation < 0) {
             return;
         }
-        if (currentStation > 9) {
+        if (currentStation > maxStation) {
             return;
         }
 
